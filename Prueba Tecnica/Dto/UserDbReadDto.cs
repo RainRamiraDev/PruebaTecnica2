d2f86b847341e13c4efa67b118bf6a10cbb7dc4c
RainRamiraDev <114005@tecnicatura.frc.utn.edu.ar>
2024-09-28 21:51:34 -0300

    .

--- Prueba Tecnica/Dto/UserDbReadDto.cs
+++ Prueba Tecnica/Dto/UserDbReadDto.cs
@@ -3,8 +3,10 @@
     public class UserDbReadDto //Dto de modificacion de BD (GetAll,GetById)
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Nombre { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Contraseña { get; set; } = null!;
     }
 }
